b38c1672caf01369cf228ca6d1e79e105de37dc9
TheBlackReverand_cp <SND\TheBlackReverand_cp>
2012-11-22 16:48:48 +0000

    Test Unitaire

---
+++ TestUnitaire/Models/DossierViewModelTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tagomatique.Models;
+
+namespace TestUnitaire.Models
+{
+	[TestClass]
+	public class DossierViewModelTest
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			DossierViewModel dossier = new DossierViewModel();
+			dossier.Nom = "Dossier 1";
+			dossier.Chemin = @"C:\";
+
+		}
+	}
+}
