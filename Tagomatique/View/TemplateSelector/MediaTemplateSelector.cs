df5ff3e88a04f6dd6ebe70026d6573f0de076489
TheBlackReverand_cp <SND\TheBlackReverand_cp>
2012-12-28 23:38:15 +0000

    Debut gestion "Liste Media correspondant"

---
+++ Tagomatique/View/TemplateSelector/MediaTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Tagomatique.Models;
+
+namespace Tagomatique.View.TemplateSelector
+{
+	public class MediaTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate PhotoTemplate { get; set; }
+		public DataTemplate StringTemplate { get; set; }
+
+		public DataTemplate OtherTemplate { get; set; }
+
+		public override DataTemplate SelectTemplate(object item, DependencyObject container)
+		{
+			MediaViewModel media = item as MediaViewModel;
+
+			if (media != null)
+			{
+				if (media.IsPhoto)
+					return PhotoTemplate;
+
+				return StringTemplate;
+			}
+
+			return OtherTemplate;
+		}
+	}
+}
