ab1eb0024e518ce1cdff4223588b66de3c3f69d1
TheBlackReverand_cp <SND\TheBlackReverand_cp>
2012-12-28 15:58:04 +0000

    mise en place classe de base pour TestUnitaire avec Init&Cleanup

---
+++ TestUnitaire/BaseTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnitaire
+{
+	public abstract class BaseTest
+	{
+		#region Initialize and Cleanup
+
+		[ClassInitialize]
+		public virtual void ClassInitialize()
+		{
+			// Make nothing if not override
+			;
+		}
+
+		[ClassCleanup]
+		public virtual void ClassCleanup()
+		{
+			// Make nothing if not override
+			;
+		}
+
+		[TestInitialize]
+		public virtual void TestInitialize()
+		{
+			// Make nothing if not override
+			;
+		}
+
+		[TestCleanup]
+		public virtual void TestCleanup()
+		{
+			// Make nothing if not override
+			;
+		}
+
+		#endregion
+	}
+}
